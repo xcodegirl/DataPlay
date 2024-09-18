f83338aa8bd6db250ddf9074d3bf9201b0496057
Jo Hoar <joanne.hoar@gmail.com>
2024-09-18 03:37:33 +0000

    combined data, randomized weekly selection

--- DataPlay.cpp
+++ DataPlay.cpp
@@ -2,12 +2,14 @@
 //
 
 #include "DataPlay.h"
+#include <cstdlib>
 
 using namespace std;
 
 #include <json/json.h>
 #include <fstream>
 #include <string>
+#include <ctime>
 
 int main(int argc, char* argv[])
 {
@@ -20,7 +22,6 @@ int main(int argc, char* argv[])
         std::cout << "Please specify file to parse." << std::endl;
         return EXIT_FAILURE;
     }
-   // ifs.open("data.json");
 
     Json::CharReaderBuilder builder;
     builder["collectComments"] = true;
@@ -29,6 +30,43 @@ int main(int argc, char* argv[])
         std::cout << errs << std::endl;
         return EXIT_FAILURE;
     }
-    std::cout << root << std::endl;
+
+    std::vector <Json::Value> items;
+
+
+    for (Json::Value::iterator it=root.begin(); it!=root.end(); ++it) {
+        Json::Value newval = *it;
+    //bool found = false;
+    // for (std::vector<Json::Value>::iterator it2=items.begin(); it2!=items.end(); ++it2) {
+    //     Json::Value val2 = *it2;
+    //     if( val2["name"].asString().find(newval["name"].asString())!=std::string::npos || newval["name"].asString().find(val2["name"].asString())!=std::string::npos){
+    //     cout << newval["name"] << " " << val2["name"] <<std::endl;
+    //     found = true;
+    //     }
+    // }
+    //if(!found)
+        items.push_back(*it);
+    }   
+
+    std::srand(std::time(nullptr)); // use current time as seed for random generator
+
+    std::vector<std::string> menu;
+    menu.push_back("Monday");
+    menu.push_back("Tuesday");
+    menu.push_back("Wednesday");
+    menu.push_back("Thursday");
+    menu.push_back("Friday");
+    menu.push_back("Saturday");
+    menu.push_back("Sunday");
+
+    Json::Value week;
+    for(std::vector<std::string>::iterator it=menu.begin(); it!=menu.end(); ++it){
+        int index = static_cast<int>(static_cast<double>(rand() / (RAND_MAX + 1.0))*items.size());
+        week[*it] = items[index];
+        items.erase(index);
+    }
+    std::cout << week << std::endl;
+
+
     return EXIT_SUCCESS;
 }
